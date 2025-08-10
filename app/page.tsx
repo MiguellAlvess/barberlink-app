4e5d772baf9c2988165b49a464aa41b77e0d5a48
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-10 16:25:07 -0300

    feat: add barbershopitem component

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-const Home = () => {
+const Home = async () => {
+  const barberShops = await db.barbershop.findMany({})
   return (
     <div>
       <Header />
@@ -58,6 +61,15 @@ const Home = () => {
             </div>
           </CardContent>
         </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barberShops.map((barberShops) => (
+            <BarbershopItem key={barberShops.id} barbershop={barberShops} />
+          ))}
+        </div>
       </div>
     </div>
   )
