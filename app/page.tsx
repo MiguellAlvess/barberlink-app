e937bad972e9cee57ed3b07f56371a18def97726
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-10 15:42:42 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,28 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
     <div>
       <Header />
+      {/* TEXTO */}
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Miguel!</h2>
         <p>Domingo, 08 de agosto</p>
+
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="O que você procura?" />
           <Button>
             <SearchIcon />
           </Button>
         </div>
+
+        {/* BANNER */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/banner.png"
@@ -25,6 +33,31 @@ const Home = () => {
             alt="BarberLink Banner"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit rounded-xl">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barberia Show</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">08</p>
+              <p className="text-sm">16:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
