5ac9ec6bf1c0f21d9e4c7e77fbd934e2d967be18
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-11 16:03:43 -0300

    chore: add next auth setup

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,9 +1,18 @@
-import { Link, HomeIcon, CalendarIcon, LogOutIcon } from "lucide-react"
+import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from "lucide-react"
 import { quickSearchOptions } from "../_constants/search-options"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import Image from "next/image"
+import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const Sidebar = () => {
   return (
@@ -11,8 +20,35 @@ const Sidebar = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid p-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <LogInIcon size={16} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[85%]">
+            <DialogHeader className="flex items-center justify-center">
+              <DialogTitle className="font-bold">
+                Faça login na plataforma
+              </DialogTitle>
+              <DialogDescription>
+                Entre com sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-semibold">
+              <Image
+                src="/google.svg"
+                alt="Google Logo"
+                width={16}
+                height={16}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage
             src={
               "https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
@@ -22,10 +58,10 @@ const Sidebar = () => {
         <div>
           <p className="font-bold">Miguel Alves</p>
           <p className="text-xs">miguelalves@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
-      <div className="flex flex-col gap-2 border-b border-solid py-5">
+      <div className="flex flex-col gap-2 border-b border-solid py-3">
         <SheetClose asChild>
           <Button className="justify-start gap-2" variant="ghost" asChild>
             <Link href="/">
@@ -39,7 +75,7 @@ const Sidebar = () => {
           Agendamentos
         </Button>
       </div>
-      <div className="flex flex-col gap-4 border-b border-solid py-5">
+      <div className="flex flex-col gap-4 border-b border-solid py-3">
         {quickSearchOptions.map((option) => (
           <Button
             className="justify-start gap-2"
@@ -56,7 +92,7 @@ const Sidebar = () => {
           </Button>
         ))}
       </div>
-      <div className="flex flex-col gap-4 py-5">
+      <div className="flex flex-col gap-4 py-3">
         <Button variant="secondary" className="gap-2">
           <LogOutIcon size={16} />
           Sair da conta
