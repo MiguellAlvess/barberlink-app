4575566628b9924b7f46d2e35361584ceceb4a55
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-14 18:24:36 -0300

    feat: add getBarbershops data access function

---
+++ app/_data_access/barbershop/get-barbershops.ts
@@ -0,0 +1,9 @@
+import "server-only"
+
+import { db } from "@/app/_lib/prisma"
+import { Barbershop } from "@prisma/client"
+
+export const getBarbershops = async (): Promise<Barbershop[]> => {
+  const barbershops = await db.barbershop.findMany({})
+  return barbershops
+}
